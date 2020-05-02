2a61688fe911c2b24808b5807577f9d2e43d66be
Tony Rim <duc_thinh_03@yahoo.com>
2020-05-02 20:09:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   end 
   
   def self.find_or_create_by_name(song_name)
-    self.find_by_name || self.create_by_name
+    self.find_by_name(song_name) || self.create_by_name
   end 
   
 end 
